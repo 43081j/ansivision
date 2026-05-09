ee99447b524b29d344f95270e8e146973a7ab98d
James Garbutt <43081j@users.noreply.github.com>
2026-05-09 20:41:42 +0100

    feat: add frames helper (#5)
    Adds a new `renderStringToFrames` function which basically returns the
    `frames` of the render result.

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,9 @@ export async function renderString(input: string): Promise<Renderer> {
   return Renderer.fromString(input);
 }
 
+export async function renderStringToFrames(input: string): Promise<string[]> {
+  const renderer = await renderString(input);
+  return renderer.frames;
+}
+
 export { Renderer, RenderStream };
